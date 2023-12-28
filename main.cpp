2c9417df2ebb5d2f5fcb27bb46c8a1b2eacb9dae
saucemoss <saucemossgames@gmail.com>
2023-12-28 15:24:47 +0100

    day3

--- main.cpp
+++ main.cpp
@@ -96,9 +96,9 @@ struct rgb_count
 
 			while (found != std::string::npos)
 			{
-				if (s == "red") r += std::stoi(line.substr(found -3, found-1));
-				if (s == "green") g += std::stoi(line.substr(found - 3, found-1));
-				if (s == "blue") b += std::stoi(line.substr(found - 3, found-1));
+				if (s == "red") r += std::stoi(line.substr(found - 3, found - 1));
+				if (s == "green") g += std::stoi(line.substr(found - 3, found - 1));
+				if (s == "blue") b += std::stoi(line.substr(found - 3, found - 1));
 				line = line.replace(line.find(s), s.length(), "");
 				found = line.find(s);
 			}
@@ -108,7 +108,6 @@ struct rgb_count
 	std::vector<std::string> strs = {
 		"red","green","blue" };
 };
-
 
 void day2p1()
 {
@@ -125,7 +124,7 @@ void day2p1()
 		bool over_rgb_count = false;
 		std::cout << "\n-------------------------------------";
 		std::cout << "\nID: " << game_id << std::endl;
-		
+
 		std::size_t find_semicolon = line.find(";");
 
 		std::string first_subset = line.substr(find_colon + 1, find_semicolon - (find_colon + 1));
@@ -161,6 +160,283 @@ void day2p1()
 void day2p2()
 {
 
+	std::ifstream infile("day2input.txt");
+	std::string line;
+	int power_sum = 0;
+
+	while (std::getline(infile, line))
+	{
+
+		int game_red = 0, game_green = 0, game_blue = 0;
+
+		std::size_t find_colon = line.find(":");
+		int game_id = std::stoi(line.substr(4, find_colon));
+
+		std::size_t find_semicolon = line.find(";");
+
+		std::string first_subset = line.substr(find_colon + 1, find_semicolon - (find_colon + 1));
+		rgb_count rgb(first_subset);
+
+		game_red = rgb.r;
+		game_green = rgb.g;
+		game_blue = rgb.b;
+
+		while (find_semicolon != std::string::npos)
+		{
+			line = line.substr(find_semicolon + 1, line.length());
+			find_semicolon = line.find(";");
+
+			std::string subset = line.substr(0, find_semicolon);
+			rgb_count rgb(subset);
+
+			rgb.r > game_red ? game_red = rgb.r : true;
+			rgb.g > game_green ? game_green = rgb.g : true;
+			rgb.b > game_blue ? game_blue = rgb.b : true;
+		}
+
+		std::cout << "\nGame " << game_id << " top values" << "\n";
+		std::cout << "Red: " << game_red << "\n";
+		std::cout << "Green: " << game_green << "\n";
+		std::cout << "Blue: " << game_blue << "\n";
+
+		power_sum += game_red * game_green * game_blue;
+	}
+
+	std::cout << "Sum of all powers: " << power_sum;
+}
+
+bool check_for_symbol_neighbour(std::vector<std::vector<char>>& arr, int y, int x)
+{
+	// Check top
+	if (y - 1 >= 0 && !isdigit(arr[y - 1][x]) && arr[y - 1][x] != '.')
+		return true;
+
+	// Check top-right
+	if (y - 1 >= 0 && x + 1 < arr[y].size() && !isdigit(arr[y - 1][x + 1]) && arr[y - 1][x + 1] != '.')
+		return true;
+
+	// Check right
+	if (x + 1 < arr[y].size() && !isdigit(arr[y][x + 1]) && arr[y][x + 1] != '.')
+		return true;
+
+	// Check bottom-right
+	if (y + 1 < arr.size() && x + 1 < arr[y].size() && !isdigit(arr[y + 1][x + 1]) && arr[y + 1][x + 1] != '.')
+		return true;
+
+	// Check bottom
+	if (y + 1 < arr.size() && !isdigit(arr[y + 1][x]) && arr[y + 1][x] != '.')
+		return true;
+
+	// Check bottom-left
+	if (y + 1 < arr.size() && x - 1 >= 0 && !isdigit(arr[y + 1][x - 1]) && arr[y + 1][x - 1] != '.')
+		return true;
+
+	// Check left
+	if (x - 1 >= 0 && !isdigit(arr[y][x - 1]) && arr[y][x - 1] != '.')
+		return true;
+
+	// Check top-left
+	if (y - 1 >= 0 && x - 1 >= 0 && !isdigit(arr[y - 1][x - 1]) && arr[y - 1][x - 1] != '.')
+		return true;
+
+	return false;
+}
+
+std::vector<int> get_digit_neighbour_coords(std::vector<std::vector<char>>& arr, int y, int x)
+{
+	int neighbour_count = 0;
+	std::vector<int> n_coords;
+
+	// Check top-left
+	if (y - 1 >= 0 && x - 1 >= 0 && isdigit(arr[y - 1][x - 1]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y - 1);
+		n_coords.push_back(x - 1);
+	}
+
+	// Check top
+	if (y - 1 >= 0 && isdigit(arr[y - 1][x]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y - 1);
+		n_coords.push_back(x);
+	}
+
+	// Check top-right
+	if (y - 1 >= 0 && x + 1 < arr[y].size() && isdigit(arr[y - 1][x + 1]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y - 1);
+		n_coords.push_back(x + 1);
+	}
+
+	// Check bottom-left
+	if (y + 1 < arr.size() && x - 1 >= 0 && isdigit(arr[y + 1][x - 1]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y + 1);
+		n_coords.push_back(x - 1);
+	}
+
+	// Check bottom
+	if (y + 1 < arr.size() && isdigit(arr[y + 1][x]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y + 1);
+		n_coords.push_back(x);
+	}
+
+	// Check bottom-right
+	if (y + 1 < arr.size() && x + 1 < arr[y].size() && isdigit(arr[y + 1][x + 1]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y + 1);
+		n_coords.push_back(x + 1);
+	}
+
+	// Check left
+	if (x - 1 >= 0 && isdigit(arr[y][x - 1]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y);
+		n_coords.push_back(x - 1);
+	}
+
+	// Check right
+	if (x + 1 < arr[y].size() && isdigit(arr[y][x + 1]))
+	{
+		neighbour_count++;
+		n_coords.push_back(y);
+		n_coords.push_back(x + 1);
+	}
+
+	return n_coords;
+
+}
+
+int get_number_from_coords(std::vector<std::vector<char>>& arr, int& y, int& x)
+{
+	std::string num_str = "";
+	int temp_x = x;
+	while (temp_x < arr[y].size() && std::isdigit(arr[y][temp_x]))
+	{
+		num_str.push_back(arr[y][temp_x]);
+		temp_x += 1;
+	}
+
+	temp_x = x - 1;
+
+	while (temp_x > 0 && std::isdigit(arr[y][temp_x]))
+	{
+		std::string::iterator it = num_str.begin();
+		num_str.insert(it, arr[y][temp_x]);
+		temp_x -= 1;
+	}
+
+	std::cout << "\n" << num_str;
+	return std::stoi(num_str);
+
+}
+
+void day3p1()
+{
+	//load char 2d array
+	std::vector<std::vector<char>> arr;
+	std::ifstream infile("day3input.txt");
+	std::string line;
+	std::vector<char> line_values;
+	int total = 0;
+
+	while (std::getline(infile, line))
+	{
+		std::vector<char> line_values;
+		for (char& x : line)
+		{
+			line_values.push_back(x);
+		}
+		arr.push_back(line_values);
+	}
+
+	//nested for
+	for (int y = 0; y < arr.size(); y++)
+	{
+		bool has_symbol = false;
+		for (int x = 0; x < arr[y].size(); x++)
+		{
+			std::string num_string;
+			int digit_counter = 0;
+
+			while ((x + digit_counter < arr[y].size()) && isdigit(arr[y][x + digit_counter]))
+			{
+				if (!has_symbol) has_symbol = check_for_symbol_neighbour(arr, y, x + digit_counter);
+				num_string.push_back(arr[y][x + digit_counter]);
+				digit_counter++;
+			}
+			x += digit_counter;
+			if (!num_string.empty() && has_symbol)
+			{
+				total += std::stoi(num_string);
+				has_symbol = false;
+			}
+		}
+
+	}
+
+	std::cout << "\ntotal: " << total << std::endl;
+
+}
+
+void day3p2()
+{
+	//load char 2d array
+	std::vector<std::vector<char>> arr;
+	std::ifstream infile("day3input.txt");
+	std::string line;
+	std::vector<char> line_values;
+	int total = 0;
+
+	while (std::getline(infile, line))
+	{
+		std::vector<char> line_values;
+		for (char& x : line)
+		{
+			line_values.push_back(x);
+		}
+		arr.push_back(line_values);
+	}
+
+	//nested for
+	for (int y = 0; y < arr.size(); y++)
+	{
+		for (int x = 0; x < arr[y].size(); x++)
+		{
+			if (arr[y][x] == '*')
+			{
+				std::vector<int> neighbours = get_digit_neighbour_coords(arr, y, x);
+
+				if (neighbours.size() == 4)
+				{
+					for (int i = 0; i < neighbours.size(); i += 2)
+					{
+						std::cout << neighbours[i] << ", " << neighbours[i + 1] << ":";
+					}
+					std::cout << "\n";
+					/*std::cout << "\n\n" << "COORDS: " << neighbours[0] <<"," << neighbours[1] << "--" << neighbours[2] << "," << neighbours[3];
+					total += get_number_from_coords(arr, neighbours[0], neighbours[1]) * get_number_from_coords(arr, neighbours[2], neighbours[3]);*/
+				}
+			}
+		}
+	}
+
+	std::cout << "\ntotal: " << total << std::endl;
+
+
+	//find gear
+	//check if 2 neighbours exist - returns two sets of y,x
+	//get full number returns number based on y,x
+	//multipy all
+
 }
 
 int main() {
@@ -168,7 +444,9 @@ int main() {
 	//day1p1();
 	//day1p2();
 	//day2p1();
-	day2p2();
+	//day2p2();
+	//day3p1();
+	day3p2();
 
 	return 0;
 }
